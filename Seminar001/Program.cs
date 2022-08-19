e1603fb690b3fc49b7050ae195aefce5b8061116
«Denis» <tytdenis2@gmail.com>
2022-08-19 13:43:43 +0500

    add Seminar 001

---
+++ Seminar001/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите два числа: ");
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+
+int pow = Convert.ToInt32(Math.Pow(numberB, 2));
+
+if(numberA == pow)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+Console.WriteLine("Первое число НЕ является квадратом второго");
+}
