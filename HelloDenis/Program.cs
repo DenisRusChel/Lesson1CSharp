55315531270a51570c9a68f1174ec37cb619b226
«Denis» <tytdenis2@gmail.com>
2022-08-22 17:11:24 +0500

    add HelloDenis

---
+++ HelloDenis/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+
+
+if(username.ToLower() == "денис")
+{
+    System.Console.WriteLine("УРА ЭТО ЖЕ ДЕНИС!!!!!");
+}
+else
+{
+    System.Console.Write("Привет, ");
+    System.Console.WriteLine(username);
+}
