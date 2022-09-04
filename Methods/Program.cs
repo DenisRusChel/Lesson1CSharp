611349b63912a42826c4770d5c9f14f7f29638bd
«Denis» <tytdenis2@gmail.com>
2022-09-04 13:58:09 +0500

    add RecursionAlgorithm

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -119,29 +119,53 @@ void PrintArray(int[] array)
     System.Console.WriteLine();
 }
 
-void SelectionSort(int[] array)
+// void SelectionSort(int[] array)
+// {
+
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if(array[j] < array[minPosition]) minPosition = j;
+//         }
+
+
+//         int temporary = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temporary;
+    
+//     }
+// }
+
+// PrintArray(arr);
+// SelectionSort(arr);
+
+// PrintArray(arr);
+
+void SelectionSortMax(int[] array)
 {
 
     for (int i = 0; i < array.Length - 1; i++)
     {
-        int minPosition = i;
+        int maxPosition = i;
 
         for (int j = i + 1; j < array.Length; j++)
         {
-            if(array[j] < array[minPosition]) minPosition = j;
+            if(array[j] > array[maxPosition]) maxPosition = j;
         }
 
 
         int temporary = array[i];
-        array[i] = array[minPosition];
-        array[minPosition] = temporary;
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
     
     }
 }
 
 PrintArray(arr);
-SelectionSort(arr);
-
+SelectionSortMax(arr);
 PrintArray(arr);
 
 
---
+++ Project/Program.cs
@@ -0,0 +1,138 @@
+﻿// 1.1 Вывести на экран число  с точностью до сотых.
+
+// double a = 15.486546;
+// a = Math.Round(a, 2);
+// System.Console.WriteLine($"Число с точность до сотых {a}");
+
+// 1.3 Составить программу вывода на экран числа, вводимого с клавиатуры. 
+// Выводимому числу должно предшествовать сообщение «Вы ввели число».
+
+// System.Console.Write("Введите число: ");
+// int digit = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine($"Вы ввели чило: {digit}");
+
+// 1.5 Вывести на одной строке числа 1, 13 и 49 с одним пробелом между ними
+
+// void Numbers(int a, int b, int c)
+// {
+//     System.Console.Write($"{a} {b} {c} ");
+//     System.Console.WriteLine();
+// }
+// Numbers(1, 13, 49);
+
+// int a = 1; int b = 13; int c = 49;
+// System.Console.WriteLine($"{a} {b} {c} ");
+
+// S1.6. Вывести на одной строке числа 7, 15 и 100 с двумя пробелами между ними.
+
+// int a = 7; int b = 15; int c = 100;
+// System.Console.WriteLine($"{a}  {b}  {c}");
+
+// Составить программу вывода на экран в одну строку трех любых чисел с двумя пробелами между ними.
+
+
+// int a = new Random().Next(1, 100);
+// int b = new Random().Next(1, 1000);
+// int c = new Random().Next(1, 100000);
+
+// System.Console.WriteLine($"{a}  {b}  {c}  ");
+
+// S1.8. Составить программу вывода на экран в одну строку четырех 
+// любых чисел  с одним пробелом между ними.
+
+// int a = new Random().Next(1, 10);
+// int b = new Random().Next(1, 10);
+// int c = new Random().Next(1, 10);
+// int d = new Random().Next(1, 10);
+
+// System.Console.WriteLine($"{a} {b} {c} {d}");
+
+// S1.9. Вывести на экран числа 50 и 10 одно под другим.
+
+// int a = 50; int b = 10;
+// System.Console.WriteLine($"{a}\n{b}");
+
+// S1.10. Вывести на экран числа 5, 10 и 21 одно под другим.
+
+// int a = 5; int b = 10; int c = 21;
+// System.Console.WriteLine($"{a}\n{b}\n{c}");
+
+// S1.11. Составить программу вывода на экран «столбиком» четырех любых чисел.
+
+// int a = new Random().Next(1, 100);
+// int b = new Random().Next(1, 100);
+// int c = new Random().Next(1, 100);
+// int d = new Random().Next(1, 100);
+
+// System.Console.WriteLine($"{a}\n{b}\n{c}\n{d}");
+
+// Random rnd = new Random();
+// int a = rnd.Next(1, 5);
+// int b = rnd.Next(1, 5);
+// int c = rnd.Next(1, 5);
+// int d = rnd.Next(1, 5);
+// System.Console.WriteLine($"{a}\n{b}\n{c}\n{d}");
+
+// void Digits()
+// {
+//     int a = new Random().Next(1, 100);
+//     int b = new Random().Next(1, 100);
+//     int c = new Random().Next(1, 100);
+//     int d = new Random().Next(1, 100);
+//     Console.WriteLine($"{a}\n{b}\n{c}\n{d}");
+// }
+// Digits();
+
+
+// S1.12. Составить программу вывода на экран следующей информации:
+// 5 10
+// 7 см
+
+// int a = 5; int b = 10; int c = 7;
+// string text = "см";
+
+// System.Console.WriteLine($"{a} {b}\n{c} {text}");
+// System.Console.WriteLine($"5 10\n7 см");
+
+// S1.13. Составить программу вывода на экран следующей информации:
+// 2 кг
+// 13 17
+// System.Console.WriteLine($"2 кг\n13 17");
+
+// I2.1. Если целое число m делится нацело на целое число n, то вывести на экран частное от деления, 
+// в противном случае вывести сообщение «m на n нацело не делится».
+
+// void Digits(int m, int n)
+// {
+
+// if (m % n == 0)
+// System.Console.WriteLine($"{m/n}");
+// else 
+// System.Console.WriteLine("m на n нацело не делится");
+// }
+// Digits(11, 5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
